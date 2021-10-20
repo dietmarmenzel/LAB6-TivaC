dc57e091287164bf66c99cc07fff2d41d0b56ba5
dietmarmenzel <89163030+dietmarmenzel@users.noreply.github.com>
2021-10-20 01:16:41 -0600

    definicion de pines para los leds y botones de la tivs

--- main.cpp
+++ main.cpp
@@ -9,3 +9,16 @@
 // Librerías
 //*****************************************************************************
 #include <Arduino.h>
+
+//*****************************************************************************
+// Definición de pines
+//*****************************************************************************
+//Definición del pin para el potenciómetro
+#define pot2 PD_1
+
+#define Rojo PF_1
+#define Verde PF_3 
+#define Azul PF_2
+
+#define Boton1 PF_0
+#define Boton2 PF_4
